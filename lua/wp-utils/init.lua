8c760dba07a8b85c5164e023bd63de47ea6fdb1b
ahollister <aaahollister@gmail.com>
2023-09-04 21:36:30 +0100

    Remove parenthesis from passed functions

--- lua/wp-utils/init.lua
+++ lua/wp-utils/init.lua
@@ -6,9 +6,9 @@ wp.setup = function()
 end
 
 -- Provide builtin functions to lua via require("wp-utils")
-wp.is_wp = builtin.is_wp()
-wp.is_wp_root = builtin.is_wp_root()
-wp.is_wp_subdirectory = builtin.is_wp_subdirectory()
-wp.get_wp_path = builtin.get_wp_path()
+wp.is_wp = builtin.is_wp
+wp.is_wp_root = builtin.is_wp_root
+wp.is_wp_subdirectory = builtin.is_wp_subdirectory
+wp.get_wp_path = builtin.get_wp_path
 
 return wp
