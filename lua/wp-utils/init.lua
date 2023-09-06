6bd41c17d7a8d560303965a20330676a29eb0d9c
ahollister <aaahollister@gmail.com>
2023-09-06 15:33:19 +0100

    Remove top level functions

--- lua/wp-utils/init.lua
+++ lua/wp-utils/init.lua
@@ -1,24 +1,7 @@
-local builtin = require("wp-utils.builtin")
 local wp = {}
 
 wp.setup = function()
 	require("wp-utils.command").create_command()
-end
-
-wp.is_wp = function()
-	return builtin.is_wp()
-end
-
-wp.is_wp_subdirectory = function()
-	return builtin.is_wp_subdirectory()
-end
-
-wp.is_wp_root = function()
-	return builtin.is_wp_root()
-end
-
-wp.get_wp_path = function()
-	return builtin.get_wp_path()
 end
 
 return wp
