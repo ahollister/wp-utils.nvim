02e3956f05d4535ad3f3402c47a8ac7112d14d55
ahollister <aaahollister@gmail.com>
2023-10-25 12:56:18 +0100

    Ensure WP path returned always starts with a single / to account for file managers like oil://

--- lua/wp-utils/state.lua
+++ lua/wp-utils/state.lua
@@ -1,6 +1,15 @@
 local state = {}
 
-state.cwd = vim.api.nvim_buf_get_name(0)
+local function removeCharactersUntilSlash(inputString)
+	local index = inputString:find("/")
+	if index == 1 then
+		return inputString:sub(index)
+	else
+		return "/" .. inputString:sub(index + 1)
+	end
+end
+
+state.cwd = removeCharactersUntilSlash(vim.api.nvim_buf_get_name(0))
 state.cwd_content = vim.api.nvim_exec("!ls $PWD", true)
 
 return state
